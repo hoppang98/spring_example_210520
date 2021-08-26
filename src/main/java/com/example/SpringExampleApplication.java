dc0143e3980475d2ef9fb34f6e1f71097106dc84
AzureAD\손지승 <miga98@naver.com>
2021-08-26 21:47:43 +0900

    210826 작업

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-@MapperScan(basePackages = "com.example.*") // mapper scan 추가!!!
+// @MapperScan(basePackages = "com.example.*") // mapper scan 추가!!! @Repository가 붙어있는걸 스캔 - DatabaseConfig로 이동
 // @ComponentScan(basePackages= {"com", "com.example"})
 // @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) - DB연동하면서 주석처리
 @SpringBootApplication
@@ -21,14 +13,14 @@ public class SpringExampleApplication {
 		SpringApplication.run(SpringExampleApplication.class, args);
 	}
 
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"); // resources에 mappers 패키지 만들기
-		sessionFactory.setMapperLocations(res);
-
-		return sessionFactory.getObject();
-	}
+//	@Bean
+//	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+//		sessionFactory.setDataSource(dataSource);
+//
+//		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"); // resources에 mappers 패키지 만들기
+//		sessionFactory.setMapperLocations(res);
+//
+//		return sessionFactory.getObject();
+//	} -  DatabaseConfig로 이동
 }
