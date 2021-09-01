b478ecaadc1d91fcdeea6dde73918fceec0a8e53
AzureAD\손지승 <miga98@naver.com>
2021-09-01 21:58:27 +0900

    20210901 작업

--- src/main/java/com/example/lesson04/NewUserController.java
+++ src/main/java/com/example/lesson04/NewUserController.java
@@ -1,14 +1,23 @@
 package com.example.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.lesson04.bo.NewUserBO;
+import com.example.lesson04.model.NewUser;
+
 @RequestMapping("/lesson04")
 @Controller 
 public class NewUserController {
+	
+	@Autowired
+	private NewUserBO newUserBO;
 	
 	// 요청 URL : http://localhost/lesson04/ex01/1
 	@RequestMapping(method = RequestMethod.GET, path = "/ex01/1") // get만 받겠다
@@ -27,8 +36,22 @@ public class NewUserController {
 			@RequestParam(value = "introduce", required=false) String introduce) { // 비필수 parameter
 		
 		// DB Insert
-		
+		newUserBO.addNewUser(name, yyyymmdd, email, introduce);
 		
 		return "lesson04/afterAddUser";
+	}
+	
+	
+	// 마지막에 가입된 사람 하나의 정보를 가지고 온다. - jsp에서 테이블로 뿌리기
+	// 요청 url : http://localhost/lesson04/ex01/last_user
+	@GetMapping("/ex01/last_user")
+	public String getLastUser(Model model) {
+		// 마지막에 가입된 사람 하나의 정보를 가지고 온다. select DB
+		NewUser user = newUserBO.getLastNewUser();
+		
+		model.addAttribute("user", user);
+		model.addAttribute("title", "회원 정보");
+		
+		return "lesson04/lastUser"; // 결과 jsp
 	}
 }
---
+++ src/main/java/com/example/lesson04/bo/NewUserBO.java
@@ -0,0 +1,23 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.dao.NewUserDAO;
+import com.example.lesson04.model.NewUser;
+
+@Service
+public class NewUserBO {
+	
+	@Autowired
+	private NewUserDAO newUserDAO;
+	
+	//select - void(리턴값 없음, 수행만)            insert, update, delete - int(몇행이 성공했는지)
+	public void addNewUser(String name, String yyyymmdd, String email, String introduce) {
+		newUserDAO.insertNewUser(name, yyyymmdd, email, introduce);
+	}
+	
+	public NewUser getLastNewUser() {
+		return newUserDAO.selectLastNewUser();
+	}
+}
